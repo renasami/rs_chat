e488f26e5c4b25be7b1bc60312a1ee854c84eacf
renasami <drrr0502@gmail.com>
2025-01-31 01:29:51 +0900

    update user

--- backend/migrations/20250128121206_create_refresh_token_table.sql
+++ backend/migrations/20250128121206_create_refresh_token_table.sql
@@ -1,7 +1,7 @@
 -- Add migration script here
 CREATE TABLE refresh_tokens (
     id UUID PRIMARY KEY,
-    user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+    user_id UUID NOT NULL UNIQUE REFERENCES users(user_id) ON DELETE CASCADE,
     token TEXT UNIQUE NOT NULL,
     expires_at TIMESTAMP NOT NULL
 );
